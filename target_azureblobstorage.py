6bd304b7d69487077fedde32f1b15d2ea42d9e53
Torstein Langan <torsteinlangan@gmail.com>
2023-07-19 18:12:26 +0200

    Add fix for python 3.10

--- target_azureblobstorage.py
+++ target_azureblobstorage.py
@@ -36,7 +36,7 @@ def flatten(d, parent_key='', sep='__'):
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, collections.abc.MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
         else:
             items.append((new_key, str(v) if type(v) is list else v))
